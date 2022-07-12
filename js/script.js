91e02a5cc9ace755c90566883b320260592c7c3a
Enciso Fernando Ariel <fernandoenciso97@gmail.com>
2022-07-12 14:11:48 -0300

    Incorporar Arrays

--- js/script.js
+++ js/script.js
@@ -1,63 +1,115 @@
-let controlIngresos = 0;
-let controlEgresos = 0;
 
-function saldo(ingresos,egresos){
-    return ingresos - egresos;
+totalIngresos = 0;
+totalEgresos = 0;
+
+
+// FUNCIONES PARA SUMAR
+
+function sumarIngresos() {
+    for (let i = 0; i < misIngresos.length; i++) {
+        totalIngresos = totalIngresos + misIngresos[i].monto
+    }
+    return totalIngresos;
 }
 
-// INGRESOS DEL USUARIO
+function sumarEgresos() {
+    for (let i = 0; i < misEgresos.length; i++) {
+        totalEgresos = totalEgresos + misEgresos[i].monto
+    }
+    return totalEgresos;
+}
 
-for(let i=1; i<11; i++){
-    alert("Descripción ingreso N° " + i)
+// CLASE CONSTRUCTORA DE OBJETOS
+
+class Dinero {
+    constructor(concepto, monto) {
+        this.concepto = concepto.toUpperCase();
+        this.monto = parseInt(monto);
+    }
+}
+
+// ARRAYS
+
+const misIngresos = [];
+const misEgresos = [];
+
+// CARGA DE INGRESOS
+
+for (let i = 1; i < 11; i++) {
+    alert("Concepto del Ingreso N° " + i)
     let conceptoIngreso = prompt("Concepto del ingreso. Si desea omitir, escriba " +"ESC");
     conceptoIngreso = conceptoIngreso.toUpperCase();
-
-    if (conceptoIngreso != "ESC"){
-        let montoIngreso = prompt("Monto del ingreso:");
-        controlIngresos = controlIngresos + parseInt(montoIngreso);
-
-        while (isNaN(montoIngreso) == true || parseInt(montoIngreso) < 0){
+    
+    if (conceptoIngreso != "ESC") {
+        let montoIngreso = prompt("Monto del Ingreso:");
+        
+        while (isNaN(montoIngreso) == true || parseInt(montoIngreso) < 0) {
             alert("ERROR. Sólo los carácteres numéricos mayores a 0 son válidos.");
             montoIngreso = prompt("Coloque nuevamente el monto del ingreso:");
-            if (isNaN(montoIngreso) == false && (parseInt(montoIngreso) > 0)){
-                controlIngresos = controlIngresos + parseInt(montoIngreso);
+            if (isNaN(montoIngreso) == false && (parseInt(montoIngreso) > 0)) {
                 break;
             }
         }
+        misIngresos.push(new Dinero(conceptoIngreso, montoIngreso));
     }
-    else{
+    else {
         break;
     }
 }
 
-// GASTOS DEL USUARIO
+// CARGA DE EGRESOS
 
-for(let i=1; i<11; i++){
-    alert("Descripción del gasto N° " + i)
+for (let i = 1; i < 11; i++) {
+    alert("Concepto del Gasto N° " + i)
     let conceptoGasto = prompt("Concepto del gasto. Si desea omitir, escriba " +"ESC");
     conceptoGasto = conceptoGasto.toUpperCase();
-
-    if (conceptoGasto != "ESC"){
-        let montoEgresos = prompt("Monto del gasto:");
-        controlEgresos = controlEgresos + parseInt(montoEgresos);
-
-        while (isNaN(montoEgresos) == true || parseInt(montoEgresos) < 0){
+    
+    if (conceptoGasto != "ESC") {
+        let montoGasto = prompt("Monto del gasto:");
+        
+        while (isNaN(montoGasto) == true || parseInt(montoGasto) < 0) {
             alert("ERROR. Sólo los carácteres numéricos mayores a 0 son válidos.");
-            montoEgresos = prompt("Coloque nuevamente el monto del gasto:");
-            if (isNaN(montoEgresos) == false && (parseInt(montoEgresos) > 0)){
-                controlEgresos = controlEgresos + parseInt(montoEgresos);
+            montoGasto = prompt("Coloque nuevamente el monto del gasto:");
+            if (isNaN(montoGasto) == false && (parseInt(montoGasto) > 0)) {
                 break;
             }
         }
+        misEgresos.push(new Dinero(conceptoGasto, montoGasto));
     }
-    else{
+    else {
         break;
     }
 }
 
-// SALDOS TOTALES
+
+// MUESTRA DE SALDOS
+
+sumarIngresos();
+sumarEgresos();
+let saldo = totalIngresos - totalEgresos;
 
 alert(
-    "Tus ingresos son de: $" + controlIngresos +"\n"+
-    "Tus gastos de este mes, suman: $" +controlEgresos +"\n"+
-    "Tu saldo disponible, es de: $" + saldo(controlIngresos,controlEgresos));
+    "Tus ingresos totales del mes son: $" + totalIngresos +"\n"+
+    "Tus gastos de este mes, suman: $" +totalEgresos +"\n"+
+    "Por lo tanto, tu saldo es de: $" + saldo);
+
+
+// MOSTRAR INFORMACIÓN DETALLADA
+
+let mensajeIngresos = "";
+for (let i = 0 ; i < misIngresos.length; i++){
+    mensajeIngresos += "Concepto: " +misIngresos[i].concepto + " | Monto: $" +misIngresos[i].monto + '\n';
+}
+
+let mensajeEgresos = "";
+for (let i = 0 ; i < misEgresos.length; i++){
+    mensajeEgresos += "Concepto: " +misEgresos[i].concepto + " | Monto: $" +misEgresos[i].monto + '\n';
+}
+
+alert(
+    "\n"
+    +"Tus Ingresos:" +"\n" 
+    +mensajeIngresos +"\n" 
+    +"\n"
+    +"Tus Egresos" +"\n"
+    +mensajeEgresos)
